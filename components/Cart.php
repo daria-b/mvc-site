83b8700409d087974b0e4cc520132fd355a7b97d
Dasha Bilan <darja-b@ukr.net>
2016-03-01 02:17:59 +0200

    site4

--- components/Cart.php
+++ components/Cart.php
@@ -63,4 +63,23 @@ class Cart
 
         return $total;
     }
+
+    public static function clear()
+    {
+        if(isset($_SESSION['products'])){
+            unset($_SESSION['products']);
+        }
+    }
+
+    public static function deleteProduct($id)
+    {
+        // Получаем массив с идентификаторами и количеством товаров в корзине
+        $productsInCart = self::getProducts();
+
+        // Удаляем из массива элемент с указанным id
+        unset($productsInCart[$id]);
+
+        // Записываем массив товаров с удаленным элементом в сессию
+        $_SESSION['products'] = $productsInCart;
+    }
 }
--- config/routes.php
+++ config/routes.php
@@ -1,27 +1,34 @@
 <?php
 
 return array(
+    // Товар:
     'product/([0-9]+)' => 'product/view/$1', //actionView in ProductController
-
+    // Каталог:
     'catalog' => 'catalog/index', //actionIndex in CatalogController
-
+    // Категория товаров:
     'category/([0-9]+)/page-([0-9]+)' => 'catalog/category/$1/$2', //actionCategory in CatalogController
     'category/([0-9]+)' => 'catalog/category/$1', //actionCategory in CatalogController
-
+    // Корзина:
+    'cart/checkout' => 'cart/checkout', // actionAdd in CartController
+    'cart/delete/([0-9]+)' => 'cart/delete/$1', // actionDelete в CartController
     'cart/add/([0-9]+)' => 'cart/add/$1', //actionAdd in CartController
-
     'cart/addAjax/([0-9]+)' => 'cart/addAjax/$1', //actionAddAjax in CartController
     'cart' => 'cart/index', //actionIndex in CartController
-
+    // Пользователь:
     'user/register' => 'user/register',
     'user/login' => 'user/login',
     'user/logout' => 'user/logout',
-
     'cabinet/edit' => 'cabinet/edit',
     'cabinet' => 'cabinet/index',
 
-    'contacts' => 'site/contact',
+    // Управление товарами:
+    // Управление категориями:
+    // Управление заказами:
+    // Админпанель:
 
+    // О магазине
+    'contacts' => 'site/contact',
+    // Главная страница
     'index' => 'site/index', //actionIndex in SiteController
 
 );
